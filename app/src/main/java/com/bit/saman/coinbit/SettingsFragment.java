d169e04b1979f91fe2aedab9db4e619428218563
saman <sami123654>
2018-09-14 22:04:49 +0430

    #add settings and preferance

---
+++ app/src/main/java/com/bit/saman/coinbit/SettingsFragment.java
@@ -0,0 +1,29 @@
+package com.bit.saman.coinbit;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+
+    public SettingsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState,
+                                    String rootKey) {
+        setPreferencesFromResource(R.xml.preferences, rootKey);
+    }
+
+}
